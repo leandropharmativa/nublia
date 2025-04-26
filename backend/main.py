7f9e7ca81185bb646d0383939748ed1f86da44bd
leandropharmativa <leandro@pharmativa.com.br>
2025-04-26 17:07:50 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database import create_db_and_tables
 from app.routers import root, users, pacientes, agenda
 from app.routers import codigos
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
@@ -26,3 +27,28 @@ app.include_router(codigos.router)
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Nublia API",
+        version="1.0.0",
+        description="Documentação da API Nublia",
+        routes=app.routes,
+    )
+    openapi_schema["components"]["securitySchemes"] = {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT"
+        }
+    }
+    for path in openapi_schema["paths"].values():
+        for operation in path.values():
+            operation["security"] = [{"BearerAuth": []}]
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+# Ativar o esquema customizado
+app.openapi = custom_openapi
