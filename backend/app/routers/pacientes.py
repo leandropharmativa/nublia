4c878de88494dc6bdcfaf9324dd96fcf6a2cfafe
leandropharmativa <leandro@pharmativa.com.br>
2025-04-26 13:21:45 -0300

    Create pacientes.py

---
+++ backend/app/routers/pacientes.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, HTTPException
+from sqlmodel import Session, select
+from app.models import Paciente, PacienteCreate
+from app.database import engine
+
+router = APIRouter()
+
+# Rota para registrar novo paciente
+@router.post("/pacientes/")
+def create_paciente(paciente: PacienteCreate):
+    with Session(engine) as session:
+        novo_paciente = Paciente.from_orm(paciente)
+        session.add(novo_paciente)
+        session.commit()
+        session.refresh(novo_paciente)
+        return {"message": "Paciente cadastrado com sucesso!", "id": novo_paciente.id}
+
+# Rota para listar todos os pacientes cadastrados
+@router.get("/pacientes/")
+def list_pacientes():
+    with Session(engine) as session:
+        return session.exec(select(Paciente)).all()
