597b1f3140857a98c0171cd4ac3fcf8e72590e7d
leandropharmativa <leandro@pharmativa.com.br>
2025-04-27 00:33:41 -0300

    Update PrescritorDashboard.jsx

--- frontend/src/pages/PrescritorDashboard.jsx
+++ frontend/src/pages/PrescritorDashboard.jsx
@@ -128,7 +128,7 @@ export default function PrescritorDashboard() {
         {/* Área Central (Ficha ou Botão) */}
 <main className="flex-1 flex flex-col items-start overflow-hidden">
   {pacienteSelecionado ? (
-    <div className="w-[99%]">
+    <div className="w-[98%]">
       <FichaAtendimento
         paciente={pacienteSelecionado}
         onFinalizar={() => setPacienteSelecionado(null)}
