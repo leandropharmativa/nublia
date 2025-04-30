ea54f3f724914fbbe44eaba9088854a40d10044b
leandropharmativa <leandro@pharmativa.com.br>
2025-04-30 02:03:46 -0300

    Update CalendarioAgenda.jsx

--- frontend/src/components/CalendarioAgenda.jsx
+++ frontend/src/components/CalendarioAgenda.jsx
@@ -17,7 +17,7 @@ const localizer = dateFnsLocalizer({
 
 export default function CalendarioAgenda({ eventos = [], aoSelecionarSlot, aoSelecionarEvento }) {
   return (
-    <div className="h-full p-6 bg-white rounded-xl shadow overflow-hidden">
+    <div className="h-full p-6 bg-white rounded shadow overflow-hidden">
       <Calendar
         localizer={localizer}
         events={eventos}
