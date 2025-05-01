e7aa07d14d32417ddfd1c9aeda00335f331a5f56
leandropharmativa <leandro@pharmativa.com.br>
2025-04-30 23:25:52 -0300

    Update PrivateRoute.jsx

--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -1,9 +1,14 @@
-// src/routes/PrivateRoute.jsx
 import { Navigate } from 'react-router-dom'
 
 export default function PrivateRoute({ children, allowedRoles }) {
   const token = localStorage.getItem('token')
-  const user = JSON.parse(localStorage.getItem('user'))
+  let user = null
+
+  try {
+    user = JSON.parse(localStorage.getItem('user'))
+  } catch (err) {
+    localStorage.removeItem('user')
+  }
 
   if (!token || !user) return <Navigate to="/" replace />
 
