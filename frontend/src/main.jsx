26bc3106ce204c944a0468c11cb2b82c94fa8775
leandropharmativa <leandro@pharmativa.com.br>
2025-04-30 23:36:43 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'  // Aqui vamos carregar o Tailwind (vamos criar esse CSS já já)
+import './index.css'
+
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
