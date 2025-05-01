4adb2297027d18c63778c482b9754178cf636b0c
leandropharmativa <leandro@pharmativa.com.br>
2025-04-30 22:03:12 -0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Public Sans"', 'sans-serif'],
+      },
+      colors: {
+        nublia: {
+          primary: "#E0F2FE",  // azul claro
+          accent: "#BFDBFE",   // azul acinzentado
+          orange: "#fb923c",   // laranja
+          dark: "#1e3a8a",     // azul profundo
+        },
+      },
+      boxShadow: {
+        subtle: "0 1px 3px rgba(0,0,0,0.06), 0 1px 2px rgba(0,0,0,0.1)",
+      },
+    },
   },
   plugins: [],
 }
