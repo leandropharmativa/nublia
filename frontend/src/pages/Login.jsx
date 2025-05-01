879095f7738153f9862c36ab446a30d91c6ba904
leandropharmativa <leandro@pharmativa.com.br>
2025-04-30 22:50:57 -0300

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -135,7 +135,7 @@ export default function Login({ onLogin }) {
             />
           </div>
 
-          <Botao type="submit" disabled={carregando}>
+          <Botao type="submit" disabled={carregando} className="mb-3">
             {carregando && (
               <svg className="animate-spin h-5 w-5 text-blue-600 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
