584b308be5c6386d0f107c6b458db9496cfcb792
leandropharmativa <leandro@pharmativa.com.br>
2025-05-02 01:57:24 -0300

    Update PrescritorDashboard.jsx

--- frontend/src/pages/PrescritorDashboard.jsx
+++ frontend/src/pages/PrescritorDashboard.jsx
@@ -198,7 +198,7 @@ useEffect(() => {
               iconeInicio={<PlusCircle size={18} />}
               onClick={() => setMostrarBuscarPacienteModal(true)}
               full={true}
-              className="rounded-full text-white bg-nublia-accent hover:bg-blue-200"
+              className="rounded-full text-white bg-nublia-accent hover:bg-blue-100"
             />
           </div>
           <div className="flex-1 overflow-hidden">
