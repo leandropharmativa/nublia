137e8224c8daabda8138c66c46130c80c3419d40
leandropharmativa <leandro@pharmativa.com.br>
2025-05-04 18:48:13 -0300

    Update CalendarioAgenda.jsx

--- frontend/src/components/CalendarioAgenda.jsx
+++ frontend/src/components/CalendarioAgenda.jsx
@@ -51,14 +51,12 @@ function ModalFinalizado({ evento, onClose }) {
         <p><strong>Paciente:</strong> {evento.nome || evento.title}</p>
         <p><strong>Data:</strong> {data}</p>
         <p><strong>Hora agendada:</strong> {horaAgendada}</p>
-{evento.hora_atendimento && (
-  <p>
-    <strong>Hora do atendimento:</strong>{' '}
-    {typeof evento.hora_atendimento === 'string'
-      ? evento.hora_atendimento + 'h'
-      : '---'}
-  </p>
-)}
+        {evento.hora_atendimento && typeof evento.hora_atendimento === 'string' ? (
+        <p><strong>Hora do atendimento:</strong> {evento.hora_atendimento}h</p>
+        ) : (
+        <p><strong>Hora do atendimento:</strong> ---</p>
+        )}
+
 
 
         <div className="mt-4 text-right">
