610912078b7a4297ac23afa031417a23de7e7114
leandropharmativa <leandro@pharmativa.com.br>
2025-05-15 22:19:37 -0300

    Update CalendarioAgenda.jsx

--- frontend/src/components/CalendarioAgenda.jsx
+++ frontend/src/components/CalendarioAgenda.jsx
@@ -30,6 +30,10 @@ import ModalFinalizado from './ModalFinalizado'
 import ListaAgendamentosAgenda from './ListaAgendamentosAgenda'
 import { toastErro } from '../utils/toastUtils'
 
+
+const [filtroTexto, setFiltroTexto] = useState('')
+const [filtroStatus, setFiltroStatus] = useState(null)
+
 const locales = { 'pt-BR': ptBR }
 
 const localizer = dateFnsLocalizer({
