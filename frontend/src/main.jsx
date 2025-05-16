8cc398e20ca692c5a021aa72d7a96bdbba7439de
leandropharmativa <leandro@pharmativa.com.br>
2025-05-16 11:30:31 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import 'react-day-picker/dist/style.css'  // primeiro
-import './CalendarioCustom.css'           // depois, para sobrescrever
 
 import { BrowserRouter } from 'react-router-dom'
 
