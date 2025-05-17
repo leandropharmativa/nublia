f494136662a7f2468723ebb68db6cc3f81db4e52
leandropharmativa <leandro@pharmativa.com.br>
2025-05-17 19:14:45 -0300

    Update EditorModeloAnamnese.jsx

--- frontend/src/components/atendimento/EditorModeloAnamnese.jsx
+++ frontend/src/components/atendimento/EditorModeloAnamnese.jsx
@@ -360,7 +360,6 @@ useEffect(() => {
         </div>
       </div>
     </div>
-)}
     {modeloParaExcluir && (
   <div className="fixed inset-0 z-50 bg-black bg-opacity-40 flex items-center justify-center">
     <div className="bg-white rounded-xl p-6 w-full max-w-sm shadow-lg relative">
