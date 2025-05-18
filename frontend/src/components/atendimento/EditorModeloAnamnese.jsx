2b570b59e69df4330abafb7f64d3d764643791b4
leandropharmativa <leandro@pharmativa.com.br>
2025-05-17 21:31:38 -0300

    Update EditorModeloAnamnese.jsx

--- frontend/src/components/atendimento/EditorModeloAnamnese.jsx
+++ frontend/src/components/atendimento/EditorModeloAnamnese.jsx
@@ -159,7 +159,7 @@ const confirmarRemocaoElemento = () => {
     setConfirmarRemocao(null)
     setAnimandoExclusao(null)
     setAguardandoExclusao(false)
-  }, 300) // ⏱ tempo da animação
+  }, 500) // ⏱ tempo da animação
 }
 
   return (
